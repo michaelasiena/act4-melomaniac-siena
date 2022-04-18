d75347d42834c063efa6b099cbae7b402db73d52
SienaMichaela <michaelasiena74@gmail.com>
2022-04-18 14:13:16 +0800

    song 3 added

---
+++ song3.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Música</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Música</h1>
+                <p class="lead">where you can search, listen, and vibe.</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+
+                        echo "<h2 class='text-primary'>Moonlight</h2>";
+                        echo "<p class='lead'>Ariana Grande</p>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<p>The sun is setting and you're right here by my side</p>";
+                        echo "<p>And the movie is playing, but we won't be watching tonight</p>";
+                        echo "<p>Every look (Every look), every touch (Every touch)<p>";
+                        echo "<p>Makes me wanna give you my heart (Ooh)</p>";
+                        echo "<p>I be crushin' on you, baby</p>";
+                        echo "<p>Stay right where you are, 'cause</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>I never knew, I never knew</p>";
+                        echo "<p>You could hold moonlight in your hands</p>";
+                        echo "<p>'Til the night I held you</p>";
+                        echo "<p>You are my moonlight, moonlight</p>";
+                        echo "<br/>";
+                        
+                        echo "<p>[Verse 2]</p>";
+                        echo "<p>I kiss his fingertips as I'm wishing he's all mine</p>";
+                        echo "<p>He's giving me Elvis with some James Dean in his eyes</p>";
+                        echo "<p>Puts his lips (Puts his lips) on my neck (On my neck)</p>";
+                        echo "<p>Makes me wanna give him my body</p>";
+                        echo "<p>I be fallin' for you, baby</p>";
+                        echo "<p>And I just can't stop, 'cause</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus 2]</p>";
+                        echo "<p>I never knew, I never knew</p>";
+                        echo "<p>You could hold moonlight in your hands</p>";
+                        echo "<p>'Til the night I held you</p>";
+                        echo "<p>You are my moonlight, moonlight</p>";
+                        echo "<p>(Baby, I be fallin')</p>";
+                        echo "<p>You're my moonlight, moonlight</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Outro]</p>";
+                        echo "<p>He's so bossy, he makes me dance</p>";
+                        echo "<p>Tryna sit in the back of his whip and just cancel my plans</p>";
+                        echo "<p>Sweet like candy, but he's such a man</p>";
+                        echo "<p>He knows just what it does when he’s holding me tight</p>";
+                        echo "<p>And he calls me moonlight too</p>";
+                        echo "<br/>";
+                        ?>
+
+                        Go back to the Main Page <a href = "index.php">Click here</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Michaela A. Siena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+
+
+    </body>
+</html> 
