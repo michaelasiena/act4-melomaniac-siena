3df7bdc2d4fd42457b7542ade73409836fc05603
SienaMichaela <michaelasiena74@gmail.com>
2022-04-18 14:02:33 +0800

    initial files

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Música </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Música</h1>
+                <p class="lead">where you can search, listen, and vibe.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+                        echo "<h2 class='text-primary'>An Art Gallery Could Never Be As Unique As You</h2>";
+                        echo "<p class='lead'>mrld</p>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<p>Darling, don't move too much</p>";
+                        echo "<p>'Cause you might break the things that you have touched</p>";
+                        echo "<p>But let me tell you, don't go too far</p>";
+                        echo "<p>And just enjoy this artistic room of ours</p>";
+                        echo "<p>This empty canvas, that they misunderstood</p>";
+                        echo "<p>I wanna paint you in it, but I'm not good</p>";
+                        echo "<p>'Cause I wanna look at you when we are apart</p>";
+                        echo "<p>'Cause you're not just a human being, you are art</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>So darling, darling don't be scared</p>";
+                        echo "<p>'Cause even if I look everywhere</p>";
+                        echo "<p>Your colors caught my eye</p>";
+                        echo "<p>And you're my favorite sight to see</p>";
+                        echo "<br/>";
+
+                        echo "<p>It's from the way that you move</p>";
+                        echo "<p>And everything that you do</p>";
+                        echo "<p>And after that it's when I realize</p>";
+                        echo "<p>That I love you</p>";
+                        echo "<br/>";
+                        
+                        echo "<p>[Verse 2]</p>";
+                        echo "<p>You told me to look everywhere else</p>";
+                        echo "<p>But I said no, 'cause when I look at you</p>";
+                        echo "<p>My heart always melts</p>";
+                        echo "<p>So I stayed even though you're a mess</p>";
+                        echo "<p>'Cause you're like drugs and with you</p>";
+                        echo "<p>Yeah, I'm obsessed</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>So darling, darling don't be scared</p>";
+                        echo "<p>'Cause even if I look everywhere</p>";
+                        echo "<p>Your colors caught my eye</p>";
+                        echo "<p>And you're my favorite sight to see</p>";
+                        echo "<br/>";
+
+                        echo "<p>It's from the way that you move</p>";
+                        echo "<p>And everything that you do</p>";
+                        echo "<p>And after that it's when I realize</p>";
+                        echo "<p>That I love you</p>";
+                        echo "<br/>";
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Michaela A. Siena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
