4ee869a0621d725c9b82703224404beaf21ebe79
SienaMichaela <michaelasiena74@gmail.com>
2022-04-18 14:24:14 +0800

    song 4 added

---
+++ song4.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Música</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Música</h1>
+                <p class="lead">where you can search, listen, and vibe.</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+
+                        echo "<h2 class='text-primary'>Tenerife Sea</h2>";
+                        echo "<p class='lead'>Ed Sheeran</p>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<p>You look so wonderful in your dress</p>";
+                        echo "<p>I love your hair like that</p>";
+                        echo "<p>The way it falls on the side of your neck</p>";
+                        echo "<p>Down your shoulders and back</p>";
+                        echo "<p>We are surrounded by all of these lies</p>";
+                        echo "<p>And people that talk too much</p>";
+                        echo "<p>You got the kind of look in your eyes</p>";
+                        echo "<p>As if no one knows anything but us</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<p>Should this be the last thing I see</p>";
+                        echo "<p>I want you to know it's enough for me</p>";
+                        echo "<p>'Cause all that you are is all that I'll ever need</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>I'm so in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse 2]</p>";
+                        echo "<p>You look so beautiful in this light</p>";
+                        echo "<p>Your silhouette over me</p>";
+                        echo "<p>The way it brings out the blue in your eyes</p>";
+                        echo "<p>Is the Tenerife sea</p>";
+                        echo "<p>And all of the voices surrounding us here</p>";
+                        echo "<p>They just fade out when you take a breath</p>";
+                        echo "<p>Just say the word and I will disappear</p>";
+                        echo "<p>Into the wilderness</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<p>Should this be the last thing I see</p>";
+                        echo "<p>I want you to know it's enough for me</p>";
+                        echo "<p>'Cause all that you are is all that I'll ever need</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>I'm so in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Bridge]</p>";
+                        echo "<p>Lumiere darling</p>";
+                        echo "<p>Lumiere over me</p>";
+                        echo "<p>Lumiere darling</p>";
+                        echo "<p>Lumiere over me</p>";
+                        echo "<p>Lumiere darling</p>";
+                        echo "<p>Lumiere over me</p>";
+
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<p>Should this be the last thing I see</p>";
+                        echo "<p>I want you to know it's enough for me</p>";
+                        echo "<p>'Cause all that you are is all that I'll ever need</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>I'm so in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<p>So in love</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Outro]</p>";
+                        echo "<p>You look so wonderful in your dress</p>";
+                        echo "<p>I love your hair like that</p>";
+                        echo "<p>And in a moment I knew you, Beth</p>";
+                        echo "<br/>";
+                        ?>
+
+                        Go back to the Main Page <a href = "index.php">Click here</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Michaela A. Siena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+
+
+    </body>
+</html> 
