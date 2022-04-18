5bc2cc9d2edf6ab6796b52cf6a9dfb2f5dfee45d
SienaMichaela <michaelasiena74@gmail.com>
2022-04-18 14:27:43 +0800

    song 5 added

---
+++ song5.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Música</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Música</h1>
+                <p class="lead">where you can search, listen, and vibe..</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+
+                        echo "<h2 class='text-primary'>Happy w u</h2>";
+                        echo "<p class='lead'>Arthur nery ft. Jason Dhakal</p>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<p>Oh, I've been on the road for hours all alone</p>";
+                        echo "<p>Just to get to you, mm-mm-mm-mm-mm</p>";
+                        echo "<p>Love</p>";
+                        echo "<p>Oh, I could do this every day</p>";
+                        echo "<p>So come my way (Yeah)</p>";
+                        echo "<p>Say a prayer</p>";
+                        echo "<p>Say a prayer before I turn off the lights, mm-mm-mm-mm-mm, no</p>";
+                        echo "<p>Get undressed (Get undressed)</p>";
+                        echo "<p>Get undressed, there ain't no reason to hide (Reason to hide)</p>";
+                        echo "<p>'Cause not everybody's obsessed with a body</p>";
+                        echo "<p>I'm up for the beauty and light from within</p>";
+                        echo "<p>You have me already, this ain't ordinary</p>";
+                        echo "<p>My hands get unsteady every time you let me in</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>I've been attached with the feeling of love reciprocating</p>";
+                        echo "<p>And waking up with you by my side</p>";
+                        echo "<p>Stuck in the same page for ages</p>";
+                        echo "<p>But baby you're ageless</p>";
+                        echo "<p>And fate is in our hands somehow</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Post-Chorus]</p>";
+                        echo "<p>I'm happy with you</p>";
+                        echo "<p>I'm happy with you (Happy with you)</p>";
+                        echo "<p>I'm happy with you (Happy with you)</p>";
+                        echo "<p>I'm happy with you (Happy)</p>";
+                        echo "<p>I'm happy with you, babe</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse 2]</p>";
+                        echo "<p>What I'd do to have you under my sheets</p>";
+                        echo "<p>What I would do to feel your heat</p>";
+                        echo "<p>You turn a glow into a fire</p>";
+                        echo "<p>My love don't expire, you're my lifeline (Lifeline)</p>";
+                        echo "<p>You are all I crave and all I seek</p>";
+                        echo "<p>Yet I'll take it day by day, don't give a fuck 'bout the wait</p>";
+                        echo "<p>Man that's a promise, I'm bein' honest</p>";
+                        echo "<p>You, nothin' compares to me and you</p>";
+                        echo "<p>You, you are the one I belong to</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>I've been attached with the feeling of love reciprocating</p>";
+                        echo "<p>And waking up with you by my side</p>";
+                        echo "<p>Stuck in the same page for ages (Same page for ages)</p>";
+                        echo "<p>But baby you're ageless (Baby you're ageless)</p>";
+                        echo "<p>And fate is in our hands somehow</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Post-Chorus]</p>";
+                        echo "<p>I'm happy with you</p>";
+                        echo "<p>I'm happy with you (Happy with you,happy with you)</p>";
+                        echo "<p>I'm happy with you</p>";
+                        echo "<p>I'm happy with you</p>";
+                        echo "<p>I'm happy with you, babe</p>";
+                        echo "<br/>";
+
+                    
+                        ?>
+
+                        Go back to the Main Page <a href = "index.php">Click here</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Michaela A. Siena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+
+
+    </body>
+</html> 
