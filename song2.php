75e1e9abbefcc81fda5cd38334e1140c4a51869e
SienaMichaela <michaelasiena74@gmail.com>
2022-04-18 14:11:48 +0800

    song 2 added

---
+++ song2.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Música</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Música</h1>
+                <p class="lead">where you can search, listen, and vibe.</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+
+                        echo "<h2 class='text-primary'>Lemonade</h2>";
+                        echo "<p class='lead'>Jeremy Passion</p>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<p>She's my sunshine in the rain</p>";
+                        echo "<p>My Tylenol when I'm in pain yeah</p>";
+                        echo "<p>Let me tell you what she means to me<p>";
+                        echo "<p>Like a tall glass of lemonade</p>";
+                        echo "<p>When it's burning hot on summer days</p>";
+                        echo "<p>She's exactly what I need</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>She's soothing like the ocean rushing on the sand</p>";
+                        echo "<p>She takes care of me baby</p>";
+                        echo "<p>And she helps me be a better man</p>";
+                        echo "<p>She's so beautiful sometimes I stop to close my eyes</p>";
+                        echo "<p>She's exactly what I need</p>";
+                        echo "<br/>";
+                        
+                        echo "<p>[Verse 2]</p>";
+                        echo "<p>She's my smile when I'm feeling blue</p>";
+                        echo "<p>She's my good night sleep when the day is through yeah</p>";
+                        echo "<p>Let me tell you what she means to me</p>";
+                        echo "<p>She's kinda like this</p>";
+                        echo "<p>Kinda like the feeling after your first kiss</p>";
+                        echo "<p>Except that everyday she makes me feel like this</p>";
+                        echo "<p>She's exactly what I need</p>";
+                        echo "<p>Oh yeah</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>She's soothing like the ocean rushing on the sand</p>";
+                        echo "<p>She takes care of me baby</p>";
+                        echo "<p>And she helps me be a better man</p>";
+                        echo "<p>She's so beautiful sometimes I stop to close my eyes</p>";
+                        echo "<p>She's exactly what I need</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>She's soothing like the ocean rushing on the sand</p>";
+                        echo "<p>She takes care of me baby</p>";
+                        echo "<p>And she helps me be a better man</p>";
+                        echo "<p>She's so beautiful sometimes I stop to close my eyes</p>";
+                        echo "<p>She's exactly what I need</p>";
+                        echo "<br/>";
+
+                        echo "<p>She's exactly what I need</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                        Go back to the Main Page <a href = "index.php">Click here</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Michaela A. Siena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+
+
+    </body>
+</html> 
