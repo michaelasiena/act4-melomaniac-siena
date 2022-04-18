246e29664d57d237c3437c2741229f3095411f2f
SienaMichaela <michaelasiena74@gmail.com>
2022-04-18 14:08:42 +0800

    song 1 added

---
+++ song1.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Música</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Música</h1>
+                <p class="lead">where you can search, listen, and vibe.</p>
+
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <?php
+
+                        echo "<h2 class='text-primary'>Red Eye</h2>";
+                        echo "<p class='lead'>Justin Beiber (ft. Troyboi)</p>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>Hoppin' on the red-eye</p>";
+                        echo "<p>Hoppin' on the red-eye</p>";
+                        echo "<p>I can rest assured knowing you're on the other of it</p>";
+                        echo "<p>Looking forward to the descent in your route</p>";
+                        echo "<p>See you when I touch down</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<p>You're my champagne</p>";
+                        echo "<p>'Cause even if I look everywhere</p>";
+                        echo "<p>You make me pop off everywhere like confetti (Yeah)</p>";
+                        echo "<p>I know it seems strange</p>";
+                        echo "<p>I seen you last night and I miss you already (Yeah, ayy)</p>";
+                        echo "<p>My bed is depressed (Depressed)</p>";
+                        echo "<p>My pillow's a victim (Hmm, hmm)</p>";
+                        echo "<p>Got me in chills while my hands are sweaty (B-O-I)</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<p>And I gotta fly out (Ha)</p>";
+                        echo "<p>Hate you got me strung-out (Ha)</p>";
+                        echo "<p>And I gotta fly out (Hey)</p>";
+                        echo "<p>Hate you got me strung-out, oh-ooh</p>";
+                        echo "<p>Only been ten hours but my sanity is gone</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>Hoppin' on the red-eye</p>";
+                        echo "<p>Hoppin' on the red-eye</p>";
+                        echo "<p>I can rest assured knowing you're on the other of it</p>";
+                        echo "<p>Looking forward to the descent in your route</p>";
+                        echo "<p>See you when I touch down</p>";
+                        echo "<br/>";
+                        
+                        echo "<p>[Verse 2]</p>";
+                        echo "<p>Got a new job, for a big bag</p>";
+                        echo "<p>You better go get it</p>";
+                        echo "<p>You'll be gone for a few days, posin' in the heat</p>";
+                        echo "<p>I ain't gon' sweat it</p>";
+                        echo "<p>I miss your distractions</p>";
+                        echo "<p>My touch is in distress</p>";
+                        echo "<p>You're just a plane ride away from my permanent visit</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<p>And I gotta fly out (Ha)</p>";
+                        echo "<p>Hate you got me strung-out (Ha)</p>";
+                        echo "<p>And I gotta fly out (Hey)</p>";
+                        echo "<p>Hate you got me strung-out, oh-ooh</p>";
+                        echo "<p>Only been ten hours but my sanity is gone</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<p>Hoppin' on the red-eye</p>";
+                        echo "<p>Hoppin' on the red-eye</p>";
+                        echo "<p>I can rest assured knowing you're on the other of it</p>";
+                        echo "<p>Looking forward to the descent in your route</p>";
+                        echo "<p>See you when I touch down</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Bridge]</p>";
+                        echo "<p>(Hey)</p>";
+                        echo "<p>Na-na-na-na-na, na-na, na-na-na, na-na, ah-ah (Oh)</p>";
+                        echo "<p>Na-na-na-na-na, na-na, na-na-na, na-na, ah-ah (Hey)</p>";
+                        echo "<p>Na-na-na-na-na, na-na, na-na-na, na-na, ah-ah</p>";
+                        echo "<p>See you when I touch down</p>";
+                        echo "<p>(Hey)</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Outro]</p>";
+                        echo "<p>Big bag (Oh)</p>";
+                        echo "<p>Big, big</p>";
+                        echo "<p>Big bag (Hey)</p>";
+                        echo "<p>Big, big</p>";
+                        echo "<p>Big bag (Oh)</p>";
+                        echo "<p>(T-R-O-Y-B-O-I, oh)</p>";
+                        echo "<br/>"; 
+                        ?>
+
+                        Go Back to the Main Page <a href = "index.php"> Click here</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Michaela A. Siena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+
+
+    </body>
+</html> 
